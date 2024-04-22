e349a2f17f3c3eb021184cdfa4524d5caec1b3fb
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-04-22 08:23:00 -0300

    exercicios listas

---
+++ 19-04-listas/ex07.py
@@ -0,0 +1,6 @@
+"""Dado o vetor nums = [3, 11, 6, 32, 15, 22, 4, 10, 5], criar uma
+matriz 3x3 com os 3 maiores elementos na primeira linha, os 3
+elementos intermediários na segunda linha, e os elementos
+menores na terceira linha."""
+
+nums = [3, 11, 6, 32, 15, 22, 4, 10, 5]
---
+++ 19-04-listas/exemplo02b.py
@@ -0,0 +1,11 @@
+list1 = []
+list2 = []
+list3 = []
+for i in range(5):
+    e1 = input(f"Digite o {i + 1}º elemento da lista 1: ")
+    e2 = input(f"Digite o {i + 1}º elemento da lista 1: ")
+    list1.append(e1)
+    list2.append(e2)
+    list3.append(e1)
+    list3.append(e2)
+print(list3)
