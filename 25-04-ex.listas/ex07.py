d28de051856098154f5fcb227e2e7dfe92d68280
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-04-28 19:47:08 -0300

    att exercicios

--- 25-04-ex.listas/ex07.py
+++ 25-04-ex.listas/ex07.py
@@ -1,1 +1,15 @@
 """Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números"""
+nums = []
+for i in range(5):
+    num = float(input(f"Digite o {i + 1}º número: "))
+    nums.append(num)
+
+soma = sum(nums)
+multi = 1
+
+for num in nums:
+    multi *= num
+
+print(f"Números digitados: {nums}")
+print(f"Soma dos números: {soma:.0f}")
+print(f"Multiplicação dos números: {multi:.0f}")
