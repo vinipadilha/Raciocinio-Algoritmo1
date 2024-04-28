ef7f4c0ff6759247a96abd4211ef32c5907b3e06
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-04-28 17:21:57 -0300

    atualiando exercicios

---
+++ 25-04-ex.listas/ex05.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor
+PAR e os números IMPARES no vetor impar. Imprima os três vetores"""
+for i in range(20):
+    nums = []
+    int(input(f"Digite o {i + 1}º número: "))
