1c7e2bb231a189eeb3cda1872aed979a7f4b7e58
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-04-29 10:29:45 -0300

    att

--- 25-04-ex.listas/ex11.py
+++ 25-04-ex.listas/ex11.py
@@ -1,9 +1,8 @@
-"""Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos, cujos
-valores deverão ser compostos pelos elementos intercalados dos dois outros vetores"""
-
+"""Altere o programa anterior, intercalando 3 vetores de 10 elementos cada"""
 vetor1 = []
 vetor2 = []
 vetor3 = []
+vetor4 = []
 
 for i in range(10):
     num1 = float(input(f"Digite o {i + 1}º número: "))
@@ -15,8 +14,13 @@ for i in range(10):
     vetor2.append(num2)
 
 for i in range(10):
-    vetor3.append(vetor1[i])
-    vetor3.append(vetor2[i]) 
+    num3 = float(input(f"Digite o {i + 1}º número: "))
+    vetor3.append(num3)
+
+for i in range(10):
+    vetor4.append(vetor1[i])
+    vetor4.append(vetor2[i]) 
+    vetor4.append(vetor3[i]) 
 
 linha = print(f"===============================================")
 
@@ -24,6 +28,6 @@ print(f"Esse é o 1º vetor: {vetor1}")
 print(linha)
 print(f"Esse é o 2º vetor: {vetor2}")
 print(linha)
-print(f"E assim ficou os vetores intercalados: {vetor3}")
-
-
+print(f"Esse é o 3º vetor: {vetor3}")
+print(linha)
+print(f"E assim ficou os vetores intercalados: {vetor4}")
