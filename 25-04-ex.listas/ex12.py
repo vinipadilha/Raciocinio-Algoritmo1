ebee9acaf6def4d79b57b36f71934d600fcea281
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-05-02 14:11:53 -0300

    atualização

--- 25-04-ex.listas/ex12.py
+++ 25-04-ex.listas/ex12.py
@@ -1,3 +1,11 @@
 """Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de
 13 anos possuem altura inferior à média de altura desses alunos."""
+alunos = []     
+for i in range(5):
+    ida = int(input(f"Digite a idade do {i + 1}º aluno: "))
+    alt = float(input(f"Digite a altura do {i + 1}º aluno: "))
+    alunos.append([ida, alt])
 
+
+    
+        
