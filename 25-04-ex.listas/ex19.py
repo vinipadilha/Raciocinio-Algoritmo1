54d242e8cdf6a030e9c391a413f415877ca52d77
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-05-15 08:47:37 -0300

    exercicios listas

--- 25-04-ex.listas/ex19.py
+++ 25-04-ex.listas/ex19.py
@@ -15,6 +15,9 @@ opções devem ser armazenados num vetor. Após os dados terem sido completament
 deverá calcular a percentual de cada um dos concorrentes e informar o vencedor da enquete. O formato da saída
 foi dado pela empresa, e é o seguinte"""
 
+def calcularPercentual(votos, totalVotos):
+    return (votos / totalVotos) * 100
+
 print("Qual o melhor Sistema Operacional para uso em servidores?")
 print("As possíveis respostas são:")
 print("1- Windows Server")
@@ -25,12 +28,35 @@ print("5- Mac OS")
 print("6- Outro")
 
 
-def respota():
-    voto = int(input("Digite a opção escolhida: "))
+def resposta():
+    votos = [0, 0, 0, 0, 0, 0]
+    totalVotos = 0  
+    voto = int(input("Digite a opção escolhida (de 1 a 6, ou 0 para encerrar): "))
+
+while voto != 0:
     if 1 <= voto <= 6:
-        print("Você escolheu a opção", voto)
-    elif voto == 0:
-        print("O programa foi encerrado.")
+        votos[voto - 1] += 1 
+        totalVotos += 1
+        print("Voto computado para a opção", voto)
     else:
-        print("Digite um valor válido (de 0 a 6).")
-        return resposta
+        print("Digite um valor válido (de 1 a 6).")
+    voto = int(input("Digite a opção escolhida (de 1 a 6, ou 0 para encerrar): "))
+
+print("Sistema Operadional     Votos     %")
+print("===================     =====     ===")
+print("Resultado da votação:")
+print(f"Windows Server:     {votos[0]} {calcularPercentual(votos[0], totalVotos):.2f} %")
+print(f"Unix:              {votos[1]} {calcularPercentual(votos[1], totalVotos):.2f} %")
+print(f"Linux:             {votos[2]} {calcularPercentual(votos[2], totalVotos):.2f} %")
+print(f"Netware:           {votos[3]} {calcularPercentual(votos[3], totalVotos):.2f} %")
+print(f"Mac OS:            {votos[4]} {calcularPercentual(votos[4], totalVotos):.2f} %")
+print(f"Outro:             {votos[5]} {calcularPercentual(votos[5], totalVotos):.2f} %")
+print("===================     =====")
+print(f"Total               {totalVotos}")
+
+
+
+
+
+
+
