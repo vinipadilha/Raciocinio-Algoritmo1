31f810b85d4eac1b353d51a877c2e69ed66bab30
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-05-16 16:33:06 -0300

    finalizando exercicios

---
+++ 25-04-ex.listas/ex24.py
@@ -0,0 +1,14 @@
+"""Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em
+um vetor . Depois, mostre quantas vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6) e uma
+função para gerar numeros aleatórios, simulando os lançamentos dos dados"""
+
+num1 = 0
+num2 = 0
+num3 = 0
+num4 = 0
+num5 = 0
+num6 = 0
+
+
+
+
