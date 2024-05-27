ee3df70c975ed22ae2d8cab5627a2dd72a1b3012
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-05-26 23:45:09 -0300

    exercicios

--- 23-05-ex.repeticao/ex8.py
+++ 23-05-ex.repeticao/ex8.py
@@ -1,11 +1,10 @@
 #Elaborar um programa que receba um número em binário, e mostre o seu valor em
 #decimal.
 
-nB = input("Digite o número binário que deseja converter: ")
+nB = int(input("Digite o número binário que deseja converter: "))
+
 nD = 0 
 
 for i in range():
-
-
 
 
